5d1bd219dc17f36bca7d4597297cc12495adfade
Analista-CJS-Lucas <95414951+Analista-CJS-Lucas@users.noreply.github.com>
2022-02-04 18:57:07 -0300

    Update run.py

--- run.py
+++ run.py
@@ -3,5 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/nps")
-def hello_world():
-    return ("<iframe title='first_charts2 - Histórico Chamados x Estatísticas Avaliações' width='1140' height='541.25' src='https://app.powerbi.com/reportEmbed?reportId=4e19597e-3470-4e3b-bd20-e0d1a852ab84&autoAuth=true&ctid=dfb66dc4-3f3c-492c-991d-727dbd1c89d4&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D' frameborder='0' allowFullScreen='true'></iframe>")
+def NPS():
+    return ("<iframe title='first_charts2 - Histórico Chamados x Estatísticas Avaliações' width='1140' height='541.25' src='https://app.powerbi.com/reportEmbed?reportId=600c6eb9-e998-4301-b55a-28f4a8ee516d&autoAuth=true&ctid=b64427b3-01ab-49de-b497-36810b81f343&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1iLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D' frameborder='0' allowFullScreen='true'></iframe>")
+
+
+@app.route("/")
+def index():
+    return ("<h1>Clinica São José</h1>")
+
+    
