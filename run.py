87e2588a2b4cec455ca7b62b3059c10939cbdee1
Analista-CJS-Lucas <95414951+Analista-CJS-Lucas@users.noreply.github.com>
2022-02-04 17:15:38 -0300

    Add files via upload

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/nps")
 def hello_world():
     return ("<iframe title='first_charts2 - Histórico Chamados x Estatísticas Avaliações' width='1140' height='541.25' src='https://app.powerbi.com/reportEmbed?reportId=4e19597e-3470-4e3b-bd20-e0d1a852ab84&autoAuth=true&ctid=dfb66dc4-3f3c-492c-991d-727dbd1c89d4&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D' frameborder='0' allowFullScreen='true'></iframe>")
